56bce0866ce983471d541650d3151ecd6b61de2d
Eric Busboom <eric@civicknowledge.com>
2024-07-30 11:45:59 -0700

    .

---
+++ src/tina.py
@@ -0,0 +1,9 @@
+from turtle import * 
+
+def new_turtle():
+    return Turtle()
+
+    import turtle                           # Tell Python we want to work with the turtle
+    turtle.setup(width=600, height=600)     # Set the size of the window
+
+    return turtle.Turtle()                  # Create a turtle named tina
