1ebf8aa8881618b60ede1435e2c8a349c034e0ab
Eric Busboom <eric@civicknowledge.com>
2024-08-01 16:44:29 -0700

    .

--- src/jtl/__init__.py
+++ src/jtl/__init__.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import inspect
 
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
@@ -14,3 +16,14 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
+
+
+def add_path():
+    # Get the path of the file from which this function is called
+    calling_file = inspect.stack()[1].filename
+    calling_directory = os.path.dirname(os.path.abspath(calling_file))
+    
+    # Add the directory to sys.path if it's not already there
+    if calling_directory not in sys.path:
+        sys.path.append(calling_directory)
+
